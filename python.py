fc88674a462212080a6169cc61ed35081bdeca7d
faizkc0101 <faizkc0101@gmail.com>
2024-11-21 11:41:25 +0530

    Initial commit

---
+++ python.py
@@ -0,0 +1,18 @@
+'''
+1) two sum
+Given an array of integers nums and an integer target,
+return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution,
+and you may not use the same element twice.
+'''
+def twoSum( nums, target):
+        for i in range(len(nums)):
+
+            for j in range(i + 1, len(nums)):
+            
+                if nums[i] + nums[j] == target:
+                   
+                    return [i, j]
+# print(twoSum([1,2,3,4],5))
+
+
