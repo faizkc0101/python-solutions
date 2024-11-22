3cd8a52cea2aeeb8acb47d994011993166356601
faizkc0101 <faizkc0101@gmail.com>
2024-11-22 11:47:03 +0530

    11/22/24

--- python.py
+++ python.py
@@ -15,4 +15,26 @@ def twoSum( nums, target):
                     return [i, j]
 # print(twoSum([1,2,3,4],5))
 
+a = [1, 2, 3, 4, 5, 5, 3]
+b = []
+for i in a:
+    if i not in b:
+        b.append(i)
+    else:    
+        b.remove(i)
+count = len(b) 
 
+# print(a)
+# print(count)    
+# print(b)        
+
+# Given an integer x, return true if x is a 
+# palindrome
+# , and false otherwise.
+
+def isPalindrome(x):
+    x_str =str(x)
+    y=x_str[::-1]
+    return x_str == y
+
+# print(isPalindrome(342))
