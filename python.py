951fef513e41618bd1f40a7cf574e687af5a97aa
faizkc0101 <faizkc0101@gmail.com>
2024-12-10 10:35:44 +0530

    12/10/24

--- python.py
+++ python.py
@@ -62,5 +62,19 @@ def removeElement( nums, val):
         return k 
 
 # print(removeElement([1, 2, 3, 4, 4, 5, 5], 4)) 
+def searchInsert( nums, target):
 
+    left, right = 0, len(nums) - 1
+    
+    while left <= right:
+        mid = left + (right - left) // 2  
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
 
+    return left
+
+print(searchInsert([1,7,3,4,6],2))
